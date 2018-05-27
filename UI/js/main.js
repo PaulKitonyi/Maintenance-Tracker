28abc14567e28325b7bf142fb51d4bdb83841964
Paul Kitonyi <paulmucimah@gmail.com>
2018-05-27 04:01:00 +0300

    [feature #157897027]:Add view_approved page
    This is a view approved requests page
    
    [finishes #157897027]

---
+++ UI/js/main.js
@@ -0,0 +1,7 @@
+function div_show(){
+    document.getElementById('give_feedback').style.display = "block";
+}
+
+function div_hide(){
+    document.getElementById('give_feedback').style.display = "none";
+}
