ec47b8d4b18b6c124cbb27eb3043f4caff266233
Paul Kitonyi <paulmucimah@gmail.com>
2018-06-02 15:42:58 +0300

    Delete unwanted file

--- Maintenance-Tracker-API/app/models.py
+++
@@ -1,8 +0,0 @@
-class User:
-    """The user class"""
-    def __init__(self, username="", email="", password="", confirm_password=""):
-        self.username = username
-        self.email = email
-        self.password = password
-        self.confirm_password = confirm_password
-
