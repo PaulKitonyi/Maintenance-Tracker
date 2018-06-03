79b8e8cec2751e3c2cd5c09c1aac8a0e1e00ae30
Paul Kitonyi <paulmucimah@gmail.com>
2018-06-03 06:30:43 +0300

    modify paths

--- Maintenance-Tracker-API/tests/test_maintenance_tracker.py
+++ Maintenance-Tracker-API/tests/test_maintenance_tracker.py
@@ -13,30 +13,28 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 
     def test_api_can_create_request(self):
         """Test POST request"""
-        res = self.client.post('/users/requests', data=json.dumps(self.request), content_type='application/json')
+        res = self.client.post('/api/v1/users/requests', data=json.dumps(self.request), content_type='application/json')
         self.assertEqual(res.status_code, 201)
 
     def test_api_can_get_all_requests(self):
         """Test GET request"""
-        res = self.client.get('/users/requests')
+        res = self.client.get('/api/v1/users/requests')
         self.assertEqual(res.status_code, 200)
 
     def test_api_can_get_single_request(self):
         """Test GET request by id"""
-        res = self.client.get('/users/requests/1')
+        res = self.client.get('/api/v1/users/requests/1')
         self.assertEqual(res.status_code, 200)
 
     def test_api_can_edit_request(self):
         """Test PUT request"""
-        self.client.post('/users/requests', data=json.dumps(self.request), content_type='application/json')
-        res = self.client.put('/users/requests/1', data=json.dumps(self.request), content_type='application/json')
-        print(res.get_data())
+        self.client.post('/api/v1/users/requests', data=json.dumps(self.request), content_type='application/json')
+        res = self.client.put('/api/v1/users/requests/1', data=json.dumps(self.request), content_type='application/json')
         self.assertEqual(res.status_code, 200)
 
     def test_api_can_delete_request(self):
         """Test DELETE request"""
-    
-        res = self.client.delete('/users/requests/1',content_type='application/json')
+        res = self.client.delete('/api/v1/users/requests/1',content_type='application/json')
         self.assertEqual(res.status_code, 200)
 
 if __name__ == "__main__":
